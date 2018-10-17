be733e790d7d137dde460b99dc9306f3332c84ed
VLC <valentinleonchristensen@gmail.com>
2018-10-17 12:30:19 +0200

    initial commit.

---
+++ src/main/java/com/mewie/mewie/MewieApplication.java
@@ -0,0 +1,12 @@
+package com.mewie.mewie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MewieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MewieApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mewie/mewie/MewieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mewie.mewie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MewieApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
