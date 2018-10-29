1f434573298568258fd27220cc4e80ec9d67e503
VLC <valentinleonchristensen@gmail.com>
2018-10-29 09:36:09 +0100

    made sure controllers did not talk to repos

--- src/main/java/com/mewie/mewie/Controllers/ActorController.java
+++ src/main/java/com/mewie/mewie/Controllers/ActorController.java
@@ -1,10 +1,14 @@
 package com.mewie.mewie.Controllers;
 
+import com.mewie.mewie.Services.ActorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ActorController {
 
+    @Autowired
+    ActorService actorService;
 
 
 }
