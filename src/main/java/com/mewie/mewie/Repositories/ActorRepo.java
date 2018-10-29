3189c6437bfa84c182711a869c2b71d4c8034d92
Jonathan Laage Olsen <jonathan.laage.olsen@gmail.com>
2018-10-29 10:33:03 +0100

    ActorRepo

---
+++ src/main/java/com/mewie/mewie/Repositories/ActorRepo.java
@@ -0,0 +1,14 @@
+package com.mewie.mewie.Repositories;
+
+import com.mewie.mewie.Beans.Actor;
+
+import java.util.List;
+
+public interface ActorRepo {
+
+    boolean createActor(Actor actor);
+    boolean deleteActor(int index);
+    boolean updateActor(Actor actor);
+    Actor getActor(int index);
+    List<Actor> getActors();
+}
